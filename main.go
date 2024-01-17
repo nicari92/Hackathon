e45b475317690083c4a32eeae78b6d6485d78186
Andrea Stefanello <andrea.stefanello@jobrapido.com>
2024-01-17 11:12:42 +0100

    Full verification  [WIP]

--- main.go
+++ main.go
@@ -5,6 +5,7 @@ import (
 	"fmt"
 	_ "github.com/golang/protobuf/proto"
 	"google.golang.org/grpc"
+	"google.golang.org/grpc/credentials/insecure"
 	"google.golang.org/grpc/reflection"
 	pb "hackathon/protos"
 	"log"
@@ -13,6 +14,7 @@ import (
 	"regexp"
 	"strconv"
 	"strings"
+	"time"
 )
 
 type server struct {
@@ -22,7 +24,11 @@ type server struct {
 var matcher, _ = regexp.Compile("(?:[a-z0-9!#$%&'*+/=?^_`{|}~-]+(?:\\.[a-z0-9!#$%&'*+/=?^_`{|}~-]+)*|\"(?:[\\x01-\\x08\\x0b\\x0c\\x0e-\\x1f\\x21\\x23-\\x5b\\x5d-\\x7f]|\\\\[\\x01-\\x09\\x0b\\x0c\\x0e-\\x7f])*\")@(?:(?:[a-z0-9](?:[a-z0-9-]*[a-z0-9])?\\.)+[a-z0-9](?:[a-z0-9-]*[a-z0-9])?|\\[(?:(?:25[0-5]|2[0-4][0-9]|[01]?[0-9][0-9]?)\\.){3}(?:25[0-5]|2[0-4][0-9]|[01]?[0-9][0-9]?|[a-z0-9-]*[a-z0-9]:(?:[\\x01-\\x08\\x0b\\x0c\\x0e-\\x1f\\x21-\\x5a\\x53-\\x7f]|\\\\[\\x01-\\x09\\x0b\\x0c\\x0e-\\x7f])+)\\])")
 var errorMessage = "Email not valid"
 
-func (s *server) SyntaxVerification(ctx context.Context, in *pb.VerificationRequest) (*pb.VerificationResponse, error) {
+var (
+	vrfyClient pb.VrfyServiceClient = nil
+)
+
+func (s *server) SyntaxVerification(_ context.Context, in *pb.VerificationRequest) (*pb.VerificationResponse, error) {
 
 	if !matcher.MatchString(in.Email) {
 		return &pb.VerificationResponse{Valid: false, ErrorMessage: &errorMessage}, nil
@@ -30,7 +36,7 @@ func (s *server) SyntaxVerification(ctx context.Context, in *pb.VerificationRequ
 	return &pb.VerificationResponse{Valid: true}, nil
 }
 
-func (s *server) SimpleVerification(ctx context.Context, in *pb.VerificationRequest) (*pb.VerificationResponse, error) {
+func (s *server) SimpleVerification(_ context.Context, in *pb.VerificationRequest) (*pb.VerificationResponse, error) {
 	if matcher.MatchString(in.GetEmail()) {
 		_, err := net.LookupIP(strings.Split(in.Email, "@")[1])
 		if err != nil {
@@ -41,22 +47,54 @@ func (s *server) SimpleVerification(ctx context.Context, in *pb.VerificationRequ
 	return &pb.VerificationResponse{Valid: false, ErrorMessage: &errorMessage}, nil
 }
 
+func (s *server) FullVerification(ctx context.Context, in *pb.VerificationRequest) (*pb.VerificationResponse, error) {
+	r, _ := s.SimpleVerification(ctx, in)
+	if !r.Valid {
+		return &pb.VerificationResponse{Valid: false, ErrorMessage: &errorMessage}, nil
+	}
+	ctx, cancel := context.WithTimeout(context.Background(), time.Second/2)
+	defer cancel()
+	vrfyResponse, err := vrfyClient.Verify(ctx, &pb.VrfyRequest{Email: in.Email})
+	if err != nil {
+		return &pb.VerificationResponse{Valid: false, ErrorMessage: &errorMessage}, nil
+	}
+	if vrfyResponse.StatusCode != 0 {
+		return &pb.VerificationResponse{Valid: false, ErrorMessage: &errorMessage}, nil
+	}
+	return &pb.VerificationResponse{Valid: true, ErrorMessage: &errorMessage}, nil
+}
+
 func main() {
+	log.Printf("Starting server")
 	serverPort, err := strconv.Atoi(os.Getenv("SERVER_PORT"))
 	if err != nil {
-		log.Fatalf("failed to parse port: %v", err)
+		log.Fatalf("failed to parse SERVER_PORT: %v", err)
 	}
+
+	vrfyAddr := os.Getenv("VRFY_ENDPOINT")
+	vrfyPort, err := strconv.Atoi(os.Getenv("VRFY_PORT"))
+	if err != nil {
+		log.Fatalf("failed to parse VRFY_PORT: %v", err)
+	}
+
 	lis, err := net.Listen("tcp", fmt.Sprintf(":%d", serverPort))
 	if err != nil {
 		log.Fatalf("failed to listen: %v", err)
 	}
 	s := grpc.NewServer()
-	// Register Greeter on the server.
 	pb.RegisterMailVerifierServer(s, &server{})
-	// Register reflection service on gRPC server.
 	reflection.Register(s)
-	log.Printf("server listening at %v", lis.Addr())
-	if err := s.Serve(lis); err != nil {
+
+	conn, err := grpc.Dial(fmt.Sprintf("%s:%d", vrfyAddr, vrfyPort), grpc.WithTransportCredentials(insecure.NewCredentials()))
+	if err != nil {
+		log.Fatalf("did not connect: %v", err)
+	}
+	defer conn.Close()
+	vrfyClient = pb.NewVrfyServiceClient(conn)
+	log.Printf("Server listening at %v", lis.Addr())
+
+	errorSrv := s.Serve(lis)
+	if errorSrv != nil {
 		log.Fatalf("failed to serve: %v", err)
 	}
 }
